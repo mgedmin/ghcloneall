c52873dd0c4df521ce86a73b784c0869b76f6812
Marius Gedminas <marius@gedmin.as>
2016-12-28 16:20:02 +0200

    Dev version bump

--- cloneall.py
+++ cloneall.py
@@ -20,7 +20,7 @@ import requests_cache
 __author__ = 'Marius Gedminas <marius@gedmin.as>'
 __licence__ = 'MIT'
 __url__ = 'https://github.com/mgedmin/cloneall'
-__version__ = '1.4'
+__version__ = '1.5.dev0'
 
 
 # Only one of the following two can be set
