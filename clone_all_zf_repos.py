cf4ed6195ecb3936ab134a2188b44d3225accd19
Marius Gedminas <marius@gedmin.as>
2016-12-28 16:12:43 +0200

    Declare this to be 1.3

--- clone_all_zf_repos.py
+++ clone_all_zf_repos.py
@@ -15,7 +15,7 @@ import requests_cache
 __author__ = 'Marius Gedminas <marius@gedmin.as>'
 __licence__ = 'MIT'
 __url__ = 'https://github.com/mgedmin/cloneall'
-__version__ = '1.3.dev0'
+__version__ = '1.3'
 
 
 DEFAULT_ORGANIZATION = 'ZopeFoundation'
