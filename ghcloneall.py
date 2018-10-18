c3e95300265c2e374e7701a3f95e6b1e99c7722a
Marius Gedminas <marius@gedmin.as>
2018-10-19 00:40:45 +0300

    Back to development: 1.6.2

--- ghcloneall.py
+++ ghcloneall.py
@@ -34,7 +34,7 @@ import requests_cache
 __author__ = 'Marius Gedminas <marius@gedmin.as>'
 __licence__ = 'MIT'
 __url__ = 'https://github.com/mgedmin/ghcloneall'
-__version__ = '1.6.1'
+__version__ = '1.6.2.dev0'
 
 
 CONFIG_FILE = '.ghcloneallrc'
